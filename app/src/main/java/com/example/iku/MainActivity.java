7ccfa25ce616b9946f634581285485b5e86871fe
Sai nishanth K R <sainishanthkr@gmail.com>
2020-11-07 19:11:56 +0530

    task 1.1 completed

--- app/src/main/java/com/example/iku/MainActivity.java
+++ app/src/main/java/com/example/iku/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.iku;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+
+    private TextView mShowCount;
+    private int mCount=0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.show_count);
+
     }
-}import androidx.appcompat.app.AppCompatActivity;
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this,R.string.toast_message, Toast.LENGTH_SHORT);
+        toast.show();}
+
+    public void countUp(View view) {
+        mCount++;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
+
+    }
+}
